64a7d4ce1d7f112366a44d0b1e0f5474346c9b50
Sumiyabazar <Sumiya5391@gmail.com>
2025-03-27 17:54:39 +0800

    add: session 02

---
+++ week_01/sesson_02/introduce.py
@@ -0,0 +1,9 @@
+#introduce myself -- this is comment
+#introduce yourself in three lines
+# for example: your first name, lastname, age, gender
+# , occupation and hobbies
+print("minii ner Sumiyabazar")
+print("Eregtei 27 nastai")
+print("Sags togloh durtai")
+print("Am bul 3")
+print("Undur 173cm jin 78kg")
